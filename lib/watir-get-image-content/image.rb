1c9300296ecb5f3d4c224250eb0f90612c753532
Galaad Gauthier <coontail7@gmail.com>
2015-06-06 16:31:44 +0200

    Yolo avec mon diabolo

--- lib/watir-get-image-content/image.rb
+++ lib/watir-get-image-content/image.rb
@@ -4,15 +4,28 @@ module Watir
 
     def to_png_base64
       assert_exists
-      js = """var canvas = document.createElement(\"canvas\");
+      js = "var canvas = document.createElement(\"canvas\");
               canvas.width = #{self.natural_width};
               canvas.height = #{self.natural_height};
               var ctx = canvas.getContext(\"2d\");
-              ctx.drawImage(arguments[0], 0, 0);
+              ctx.drawImage({width: #{self.natural_width}, height: #{self.natural_height}}, 0, 0);
               var dataURL = canvas.toDataURL(\"image/png\");
-              return dataURL.replace(/^data:image\/(png|jpg|gif);base64,/, "");"""
+              return dataURL.replace(/^data:image\/(png|jpg|gif);base64,/, "");
+            "
 
       driver.execute_script js, @element
+
+          def to_png_base64
+      assert_exists
+      js = %q{var canvas = document.createElement("canvas");
+              canvas.width = arguments[0].width;
+              canvas.height = arguments[0].height;
+              var ctx = canvas.getContext("2d");
+              ctx.drawImage(arguments[0], 0, 0);
+              var dataURL = canvas.toDataURL("image/png");
+              return dataURL.replace(/^data:image\/(png|jpg|gif);base64,/, "");}
+      driver.execute_script js, @element
+    end
     end
 
   end
