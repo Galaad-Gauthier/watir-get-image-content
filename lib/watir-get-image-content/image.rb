008daecab545a75d0e4bed79c01c06997251c2fc
Galaad Gauthier <coontail7@gmail.com>
2015-06-06 17:24:12 +0200

    fixed ?

--- lib/watir-get-image-content/image.rb
+++ lib/watir-get-image-content/image.rb
@@ -3,14 +3,16 @@ module Watir
   class Image
 
     def to_png_base64
+      puts 
       assert_exists
       js = "var canvas = document.createElement('canvas');
               canvas.width = #{self.natural_width};
               canvas.height = #{self.natural_height};
               var ctx = canvas.getContext('2d');
-              ctx.drawImage({width: #{self.natural_width}, height: #{self.natural_height}}, 0, 0);
+              var ctx = canvas.getContext('2d');
+              ctx.drawImage($("img"), 0, 0);
               var dataURL = canvas.toDataURL('image/png');
-              dataURL.replace(/^data:image\/(png|jpg|gif);base64,/, "");
+              dataURL.replace(/^data:image\/(png|jpg|gif);base64\,/,"");
             "
 
       driver.execute_script js, @element
