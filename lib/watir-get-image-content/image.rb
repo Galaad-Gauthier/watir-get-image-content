1ecf864d2ee09fdc5f0cfa1481940466fbbcaea7
Galaad Gauthier <coontail7@gmail.com>
2015-06-13 04:06:14 +0200

    Changed

--- lib/watir-get-image-content/image.rb
+++ lib/watir-get-image-content/image.rb
@@ -5,8 +5,8 @@ module Watir
     def to_png_base64
       assert_exists
       js = "var canvas = document.createElement('canvas');
-      canvas.width = #{self.natural_width};
-      canvas.height = #{self.natural_height};
+      canvas.width = #{self.width * 1.25};
+      canvas.height = #{self.height * 1.25};
       var ctx = canvas.getContext('2d');
       ctx.drawImage(arguments[0], 0, 0);
       var dataURL = canvas.toDataURL('image/png');
